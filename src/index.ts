8f810dbb64e5875ebef04cbca92d2912257868d8
AKarp123 <ashtonkarp63@gmail.com>
2025-07-01 20:18:12 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import config from "./config";
-import { SubsonicAPI,  } from "subsonic-api";
+import { SubsonicAPI  } from "subsonic-api";
+import { startRPC, updateActivity,  } from "./rpc";
+import type { Client,  } from "@xhayper/discord-rpc";
+import { ActivityType } from "discord-api-types/v9"
+import { sleep } from "bun";
+
 
 
 const api = new SubsonicAPI({
@@ -31,21 +36,36 @@ const main = async () => {
         return;
     }
 
-    setInterval(async () => {
-        fetchNowPlaying().then((response) => {
-            if (response.nowPlaying.entry && response.nowPlaying.entry.length > 0) {
-                let userNowPlaying = response.nowPlaying.entry.filter((entry) => entry.username.toLowerCase() === config.subsonic_username.toLowerCase())[0];
-                if (userNowPlaying) {
-                    console.log(`Now Playing: ${userNowPlaying.title} by ${userNowPlaying.artist} (${userNowPlaying.album})`);
-                }
-            }
-            else {
-                console.log("No current track playing for the user.");
-            }
-        })
-    }, 1000);
-}
+    // setInterval(async () => {
+    //     fetchNowPlaying().then((response) => {
+    //         if (response.nowPlaying.entry && response.nowPlaying.entry.length > 0) {
+    //             let userNowPlaying = response.nowPlaying.entry.filter((entry) => entry.username.toLowerCase() === config.subsonic_username.toLowerCase())[0];
+    //             if (userNowPlaying) {
+    //                 console.log(`Now Playing: ${userNowPlaying.title} by ${userNowPlaying.artist} (${userNowPlaying.album})`);
+    //             }
+    //         }
+    //         else {
+    //             console.log("No current track playing for the user.");
+    //         }
+    //     })
+    // }, 1000);
+    let client : Client | undefined = undefined;
+    try{
+        client = startRPC("1389756106213163038");
+    }catch (error) {
+        console.error("Error starting RPC:", error);
+        return;
+    }
 
+    updateActivity(client, {
+        details: "Listening to music",
+        state: "Fetching now playing...",
+        type: ActivityType.Listening
+    });
+
+    await sleep(5000)
+    client.destroy();
+}
 
 if(require.main === module) {
     main().catch(console.error);
---
+++ src/rpc.ts
@@ -0,0 +1,42 @@
+import { Client, type SetActivity } from "@xhayper/discord-rpc";
+import { sleep } from "bun";
+
+
+
+const startRPC =  (discord_client_id: string) : Client => {
+    const client = new Client({
+        clientId: discord_client_id,
+        
+    });
+
+    client.on("ready", () => {
+        console.log("RPC Client is ready!");
+    });
+
+    client.on("ERROR", (error) => {
+        console.error("RPC Client Error:", error);
+        throw Error("Failed to start RPC Client");
+    });
+
+    client.login();
+
+    return client;
+}
+
+const updateActivity = async (client: Client, activity: SetActivity) => {
+
+    while(!client.isConnected) {
+        await sleep(1000);
+    }
+    try {
+        await client.user?.setActivity(activity);
+        console.log("Activity updated successfully.");
+    } catch (error) {
+        console.error("Failed to update activity:", error);
+    }
+}
+
+
+
+
+export { startRPC, updateActivity}
